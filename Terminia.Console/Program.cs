c085d1e2fde19e55de37d7ae6fc88e8a34974abd
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2022-12-19 01:55:57 -0300

    example: updated render strategy

--- Terminia.Console/Program.cs
+++ Terminia.Console/Program.cs
@@ -9,29 +9,38 @@ var gameActor = new GameActor(gameMap, '@', "#ffff");
 
 await gameActor.Initialize();
 
+render();
+
 while (true)
 {
-    var rendered = gameMap.ToString("{}");
-    Screen.Render(rendered);
-
     var control = Console.ReadKey(true);
 
     switch(control.Key)
     {
         case ConsoleKey.W:
             await gameActor.Move(0, -1);
+            render();
             break;
 
         case ConsoleKey.A:
             await gameActor.Move(-1, 0);
+            render();
             break;
 
         case ConsoleKey.S:
             await gameActor.Move(0, 1);
+            render();
             break;
 
         case ConsoleKey.D:
             await gameActor.Move(1, 0);
+            render();
             break;
     }
+}
+
+void render()
+{
+    var rendered = gameMap.ToString("{}");
+    Screen.Render(rendered);
 }
